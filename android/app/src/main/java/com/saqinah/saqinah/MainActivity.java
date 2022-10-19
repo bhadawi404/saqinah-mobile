4b0b45d5762456518524204a72766e3291302722
Fauzi Rakhman <rakhman14@gmail.com>
2022-10-20 00:50:45 +0700

    first commit

---
+++ android/app/src/main/java/com/saqinah/saqinah/MainActivity.java
@@ -0,0 +1,6 @@
+package com.saqinah.saqinah;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:saqinah/app/widget/splash.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() {
+  runApp(
+    myApp()
+  );
+}
+
+class myApp extends StatelessWidget {
+  const myApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Future.delayed(Duration(seconds: 3)),
+      builder: (context, snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return SplashScreen();
+        }else{
+          return GetMaterialApp(
+          title: "Application",
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+        );
+        }
+      }
+    );
+  }
+}
